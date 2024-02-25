86cc5e13ebfd2d4a6639c83bb574b8e2379e10a9
GitHub Action <action@github.com>
2024-02-25 10:09:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/horsewin/copyrepolink/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/horsewin/copyrepolink/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.horsewin.copyrepolink.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/horsewin/copyrepolink/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/horsewin/copyrepolink/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.horsewin.copyrepolink.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.horsewin.copyrepolink.MyBundle
+import com.github.horsewin.copyrepolink.services.MyProjectService
 import javax.swing.JButton
 
 
